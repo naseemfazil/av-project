cbe9ce2260ec83061d9db8ffa92af13e5b84ec42
Haribalan <its.hari.programmer@gmail.com>
2024-01-15 17:44:17 +0530

    Recent updates card changes

--- src/components/recentUpdates.jsx
+++ src/components/recentUpdates.jsx
@@ -3,7 +3,7 @@ import '../styles/_common.scss';
 import '../styles/_commonStyles.scss';
 import '../styles/_recentUpdates.scss';
 import Forward from '../assests/Forward and Backward Icon.svg';
-
+import TopArrow from '../assests/Top-arrow.svg';
 
 const RecentUpdates = () => {
 
@@ -14,7 +14,7 @@ const RecentUpdates = () => {
                     Recent Updates
                 </span>
                 <span>
-                    <img src={Forward} alt=' ' />
+                    {/* <img src={TopArrow} alt=' ' /> */}
                 </span>
             </div>
 
@@ -22,7 +22,7 @@ const RecentUpdates = () => {
                 <div className="first">
 
                     <div className='article-content'>
-                        <div>
+                        <div className='article-first-row'>
                             <span>
                                 <span>
                                     Article
@@ -32,34 +32,90 @@ const RecentUpdates = () => {
                                 </span>
                             </span>
                             <span>
-                                arrow
+                                <img src={TopArrow} alt=' ' />
                             </span>
                         </div>
                         <div>
-                            <span>
+                            <span className='article-head'>
                                 Things you should know about Glueblu
                             </span>
                         </div>
                         <div>
-                            <span>
-                                Step into the virtual wonderland of Glublu Community, a digital haven where enthusiasts of whimsy and fantasy converge... Read more
+                            <span className='article-body'>
+                                Step into the virtual wonderland of Glublu Community, a digital haven where enthusiasts of whimsy and fantasy converge...
+                                <span className='read-more'>
+                                    Read more
+                                </span>
                             </span>
                         </div>
                     </div>
-
-
                 </div>
 
                 <div className="second">
-                    <div className="second-one">Two one</div>
+                    <div className="second-one">
+                        <span className='top-arrow'>
+                            <span>
+                                <img src={TopArrow} alt=' ' />
+                            </span>
+                        </span>
+                        <span>
+                            circle icons
+                        </span>
+
+                        <span className='mid-text-content'>
+                            Stay connected with the updates from your community members
+                        </span>
+
+                        <span>
+                            50+ Activites
+                        </span>
+                    </div>
+
+
                     <div className="second-two">
 
                         <div className="input">
-                            input
+                            <span className='icon-row'>
+                                <span>
+                                    icon1
+                                </span>
+
+                                <span>
+                                    <img src={TopArrow} alt=' ' />
+                                </span>
+                            </span>
+
+                            <span>
+                                profile
+                            </span>
+
+                            <span>
+                                Andrew
+                            </span>
+
+                            <span className='mid-text-content'>
+                                Why do we use auto layout in figma and XD ?
+                            </span>
+                            <span>
+                                Type your answer
+                            </span>
                         </div>
 
                         <div className="story">
-                            story
+                            <span className='icon-row'>
+                                <span>
+                                    <img src={TopArrow} alt=' ' />
+                                </span>
+                            </span>
+                            <span>
+                                profile
+                            </span>
+                            <span>
+                                Create your story
+                            </span>
+                            <span>
+                                Create story
+                            </span>
                         </div>
                     </div>
                 </div>
